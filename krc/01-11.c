759ab9534724268dc4d6e0dab44b71f6ec8e17a8
Josh Mcguigan <joshmcg88@gmail.com>
2022-04-16 13:14:07 -0700

    more krc ch1

---
+++ krc/01-11.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <assert.h>
+
+#define IN 0
+#define OUT 1
+
+int wc(char* s) {
+	int wc, state;
+
+	wc = 0;
+	state = OUT;
+
+	while(*s != '\0') {
+		if(*s == ' ' || *s == '\n' || *s == '\t') {
+			state = OUT;
+		} else if (state == OUT) {
+			state = IN;
+			wc++;
+		}
+		s++;
+	}
+
+	return wc;
+}
+
+int main() {
+	assert(3 == wc("foo bar baz"));
+
+	return 0;
+}
---
+++ krc/01-12.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <assert.h>
+
+#define IN 0
+#define OUT 1
+
+int main() {
+	int c;
+	int state = OUT;
+
+	while((c = getchar()) != EOF) {
+		if(state == IN && (c == ' ' || c == '\n' || c == '\t')) {
+			state = OUT;
+			putchar('\n');
+		} else {
+			state = IN;
+			putchar(c);
+		}
+	}
+
+	return 0;
+}
